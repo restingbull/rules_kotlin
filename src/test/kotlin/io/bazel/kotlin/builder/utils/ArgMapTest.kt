8ea694266b8ee73cd98b0ec5e6f8a8ed02ff7799
Corbin Smith <corbin@squareup.com>
2020-02-28 16:17:14 -0500

    Builder supports building abis on request.

--- src/test/kotlin/io/bazel/kotlin/builder/utils/ArgMapTest.kt
+++ src/test/kotlin/io/bazel/kotlin/builder/utils/ArgMapTest.kt
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2020 The Bazel Authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
 package io.bazel.kotlin.builder.utils
 
 import com.google.common.truth.Truth
@@ -41,7 +58,7 @@ class ArgMapTest {
       override val flag = "optimist"
     }
     val args = ArgMap(mapOf(
-        Pair(empty.flag, listOf()),
+        Pair(empty.flag, emptyList()),
         Pair(full.flag, listOf("half"))
     ))
     Truth.assertThat(args.hasAll(full)).isTrue()
